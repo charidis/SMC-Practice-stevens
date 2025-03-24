4ca2b506e5a18ad48e66a09d7b36c8fae44a69a4
Charidi Stevens <92543813+charidis@users.noreply.github.com>
2025-03-24 16:36:14 -0500

    Update test.py
    removed comments from code

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ from selenium import webdriver
 
 @pytest.fixture
 def driver():
-    # Initialize the WebDriver (make sure the driver is correctly installed)
+
     driver = webdriver.Chrome()
     yield driver
     driver.quit()
